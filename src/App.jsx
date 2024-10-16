b201a08c1ba46d98ab19f691629ed1f22cbf5449
Sappakit <pipitwattana.s@gmail.com>
2024-10-16 19:20:26 +0700

    feat: add responsive hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function NavBar() {
   return (
-    <nav className="flex justify-between items-center h-20 px-10 border border-b-2 border-[--nav-bar-border-color] lg:px-36">
+    <nav className="flex justify-between items-center h-16 px-10 border border-b-2 border-[--nav-bar-border-color] lg:h-20 lg:px-36 lg:border-b">
       <p className="text-2xl font-medium lg:text-3xl">
         hh<span className="text-[--logo-accent-color]">.</span>
       </p>
@@ -27,10 +27,63 @@ function NavBar() {
   );
 }
 
+function HeroSection() {
+  return (
+    <section className="flex flex-col justify-center gap-9 px-6 py-9 lg:flex-row lg:justify-center lg:items-center lg:mt-16 lg:p-0 lg:px-16 lg:gap-16">
+      <div className="header-text flex flex-col gap-4 lg:w-[35%] lg:gap-8">
+        <h1 className="text-[2.65rem] leading-tight font-bold text-center text-[--font-primary-accent-color] lg:text-right lg:text-6xl lg:leading-none">
+          Stay <br className="hidden lg:inline" /> Informed, <br /> Stay
+          Inspired
+        </h1>
+        <p className="font-semibold text-center text-[--font-neutral-color] lg:text-right">
+          Discover a World of Knowledge at Your
+          <br className="hidden lg:inline" /> Fingertips. Your Daily Dose of
+          Inspiration <br className="hidden lg:inline" /> and Information.
+        </p>
+      </div>
+
+      <div className="flex justify-center">
+        <img
+          className="h-[30rem] w-auto rounded-2xl object-cover lg:h-[32.5rem] lg:w-[24rem]"
+          src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+          alt="man with a cat"
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 lg:w-[35%]">
+        <div className="flex flex-col gap-1">
+          <p className="text-[--font-neutral-color] text-sm">-Author</p>
+          <h2 className="text-[--font-primary-accent-color] text-3xl font-bold">
+            Thompson P.
+          </h2>
+        </div>
+
+        <div className="font-semibold text-[--font-neutral-color] flex flex-col gap-4">
+          <p>
+            I am a pet enthusiast and freelance writer
+            <br className="hidden lg:inline" /> who specializes in animal
+            behavior and <br className="hidden lg:inline" /> care. With a deep
+            love for cats, I enjoy <br className="hidden lg:inline" /> sharing
+            insights on feline companionship <br className="hidden lg:inline" />
+            and wellness.
+          </p>
+          <p>
+            When i’m not writing, I spends time
+            <br className="hidden lg:inline" /> volunteering at my local animal
+            shelter, <br className="hidden lg:inline" /> helping cats find
+            loving homes.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 function App() {
   return (
     <>
       <NavBar />
+      <HeroSection />
     </>
   );
 }
