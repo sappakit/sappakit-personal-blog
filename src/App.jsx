d85f0fea5c5796598c74aed4f1e8f391d6301d24
Sappakit <pipitwattana.s@gmail.com>
2024-10-16 17:56:26 +0700

    feat: add responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
+function NavBar() {
+  return (
+    <nav className="flex justify-between items-center h-20 px-10 border border-b-2 border-[--nav-bar-border-color] lg:px-36">
+      <p className="text-2xl font-medium lg:text-3xl">
+        hh<span className="text-[--logo-accent-color]">.</span>
+      </p>
+      <div className="flex gap-2">
+        <p className="inline text-2xl lg:hidden">--</p>
+
+        {/* Desktop button */}
+        <button
+          className="hidden h-11 px-10 rounded-full border border-[--button-border-color] font-semibold text-[--font-primary-accent-color] bg-[--button-neutral-background-color] lg:inline"
+          type="button"
+        >
+          Log in
+        </button>
+        <button
+          className="hidden h-11 px-10 rounded-full text-[--font-secondary-accent-color] bg-[--button-accent-background-color] lg:inline"
+          type="button"
+        >
+          Sign up
+        </button>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <>
-      <section>
-        <h1>My First React App on Vite</h1>
-      </section>
+      <NavBar />
     </>
   );
 }
