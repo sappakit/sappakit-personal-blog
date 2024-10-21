5464219ce8df3384db774bc17f053950bbe9372d
Sappakit <pipitwattana.s@gmail.com>
2024-10-21 13:54:48 +0700

    feat: add shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/shadcn-ui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("shadcn-ui/plugin")],
 };
