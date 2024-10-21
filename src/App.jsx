813711ac7c78bbe155246bc56f3aa9e0a8c6b40a
Sappakit <pipitwattana.s@gmail.com>
2024-10-21 13:26:45 +0700

    chore: refine hero section layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import "./App.css";
 
 function NavBar() {
   return (
-    <nav className="flex justify-between items-center h-16 px-10 border border-b-2 border-[--nav-bar-border-color] lg:h-20 lg:px-36 lg:border-b">
+    <nav className="flex justify-between items-center h-16 px-10 border border-b-2 border-[--nav-bar-border-color] lg:h-20 lg:px-36 lg:py-5 lg:border-b">
       <p className="text-2xl font-medium lg:text-3xl">
         hh<span className="text-[--logo-accent-color]">.</span>
       </p>
-      <div className="flex gap-2">
+      <div className="flex gap-2 lg:h-full">
         <p className="inline text-2xl lg:hidden">--</p>
 
         {/* Desktop button */}
         <button
-          className="hidden h-11 px-10 rounded-full border border-[--button-border-color] font-semibold text-[--font-primary-accent-color] bg-[--button-neutral-background-color] lg:inline"
+          className="hidden h-full px-10 rounded-full border border-[--button-border-color] font-semibold text-[--font-primary-accent-color] bg-[--button-neutral-background-color] lg:inline"
           type="button"
         >
           Log in
         </button>
         <button
-          className="hidden h-11 px-10 rounded-full text-[--font-secondary-accent-color] bg-[--button-accent-background-color] lg:inline"
+          className="hidden h-full px-10 rounded-full text-[--font-secondary-accent-color] bg-[--button-accent-background-color] lg:inline"
           type="button"
         >
           Sign up
@@ -29,8 +29,8 @@ function NavBar() {
 
 function HeroSection() {
   return (
-    <section className="flex flex-col justify-center gap-9 px-6 py-9 lg:flex-row lg:justify-center lg:items-center lg:mt-16 lg:p-0 lg:px-16 lg:gap-16">
-      <div className="header-text flex flex-col gap-4 lg:w-[35%] lg:gap-8">
+    <section className="flex flex-col justify-center gap-9 px-6 py-9 lg:flex-row lg:justify-center lg:items-center lg:max-w-screen-2xl lg:p-8 lg:py-20 lg:gap-16 lg:mx-auto">
+      <div className="header-text flex flex-col gap-4 lg:w-1/3 lg:gap-8">
         <h1 className="text-[2.65rem] leading-tight font-bold text-center text-[--font-primary-accent-color] lg:text-right lg:text-6xl lg:leading-none">
           Stay <br className="hidden lg:inline" /> Informed, <br /> Stay
           Inspired
@@ -42,15 +42,15 @@ function HeroSection() {
         </p>
       </div>
 
-      <div className="flex justify-center">
+      <div className="flex justify-center lg:w-1/3">
         <img
-          className="h-[30rem] w-auto rounded-2xl object-cover lg:h-[32.5rem] lg:w-[24rem]"
+          className="h-[30rem] rounded-2xl object-cover md:w-[75%] lg:w-full lg:h-[35rem]"
           src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
           alt="man with a cat"
         />
       </div>
 
-      <div className="flex flex-col gap-4 lg:w-[35%]">
+      <div className="flex flex-col gap-4 lg:w-1/3">
         <div className="flex flex-col gap-1">
           <p className="text-[--font-neutral-color] text-sm">-Author</p>
           <h2 className="text-[--font-primary-accent-color] text-3xl font-bold">
