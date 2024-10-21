02aaa40cec42ae2a0ab6471849222300d339c2be
Sappakit <pipitwattana.s@gmail.com>
2024-10-21 15:23:14 +0700

    feat: add lucide component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/shadcn-ui/dist/**/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("shadcn-ui/plugin")],
+  plugins: [],
 };
