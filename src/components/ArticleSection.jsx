ddafddf6451520afa032893706a25ecc512efaa2
Sappakit <pipitwattana.s@gmail.com>
2024-10-22 16:24:35 +0700

    refactor: use array.map on category to reduce redundancy

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -13,16 +13,37 @@ import { Search } from "lucide-react";
 import BlogCard from "./BlogCard";
 import { blogPosts } from "@/data/blogPosts";
 
-function ArticleTabsTrigger({ tabValue }) {
-  let tabName = tabValue.charAt(0).toUpperCase() + tabValue.slice(1);
+function CategoryTabsTrigger() {
+  const tabName = ["Highlight", "Cat", "Inspiration", "General"];
 
   return (
-    <TabsTrigger
-      className="rounded-md px-4 py-2 text-[--font-neutral-light-color] data-[state=active]:bg-[var(--nav-bar-background-color)] data-[state=active]:text-[--font-neutral-dark-color]"
-      value={tabValue}
-    >
-      {tabName}
-    </TabsTrigger>
+    <>
+      {tabName.map((name) => {
+        const tabValue = name.toLowerCase();
+
+        return (
+          <TabsTrigger
+            className="rounded-md px-4 py-2 text-[--font-neutral-light-color] data-[state=active]:bg-[var(--nav-bar-background-color)] data-[state=active]:text-[--font-neutral-dark-color]"
+            value={tabValue}
+          >
+            {name}
+          </TabsTrigger>
+        );
+      })}
+    </>
+  );
+}
+
+function CategorySelectItem() {
+  const tabName = ["Highlight", "Cat", "Inspiration", "General"];
+
+  return (
+    <>
+      {tabName.map((name) => {
+        const selectValue = name.toLowerCase();
+        return <SelectItem value={selectValue}>{name}</SelectItem>;
+      })}
+    </>
   );
 }
 
@@ -40,10 +61,7 @@ export function ArticleSection() {
       >
         <TabsList className="flex h-16 w-full items-center justify-between rounded-xl bg-[--bar-color] px-6">
           <div>
-            <ArticleTabsTrigger tabValue="highlight" />
-            <ArticleTabsTrigger tabValue="cat" />
-            <ArticleTabsTrigger tabValue="inspiration" />
-            <ArticleTabsTrigger tabValue="genearal" />
+            <CategoryTabsTrigger />
           </div>
 
           <div className="search-input relative w-1/4">
@@ -60,59 +78,23 @@ export function ArticleSection() {
           className="row-auto m-0 grid grid-cols-2 gap-5 pt-12"
           value="highlight"
         >
-          <BlogCard
-            image={blogPosts[0].image}
-            category={blogPosts[0].category}
-            title={blogPosts[0].name}
-            description={blogPosts[0].description}
-            author={blogPosts[0].author}
-            date={blogPosts[0].date}
-          />
-          <BlogCard
-            image={blogPosts[1].image}
-            category={blogPosts[1].category}
-            title={blogPosts[1].name}
-            description={blogPosts[1].description}
-            author={blogPosts[1].author}
-            date={blogPosts[1].date}
-          />
-          <BlogCard
-            image={blogPosts[2].image}
-            category={blogPosts[2].category}
-            title={blogPosts[2].name}
-            description={blogPosts[2].description}
-            author={blogPosts[2].author}
-            date={blogPosts[2].date}
-          />
-          <BlogCard
-            image={blogPosts[3].image}
-            category={blogPosts[3].category}
-            title={blogPosts[3].name}
-            description={blogPosts[3].description}
-            author={blogPosts[3].author}
-            date={blogPosts[3].date}
-          />
-          <BlogCard
-            image={blogPosts[4].image}
-            category={blogPosts[4].category}
-            title={blogPosts[4].name}
-            description={blogPosts[4].description}
-            author={blogPosts[4].author}
-            date={blogPosts[4].date}
-          />
-          <BlogCard
-            image={blogPosts[5].image}
-            category={blogPosts[5].category}
-            title={blogPosts[5].name}
-            description={blogPosts[5].description}
-            author={blogPosts[5].author}
-            date={blogPosts[5].date}
-          />
+          {blogPosts.map((post) => {
+            return (
+              <BlogCard
+                image={post.image}
+                category={post.category}
+                title={post.name}
+                description={post.description}
+                author={post.author}
+                date={post.date}
+              />
+            );
+          })}
         </TabsContent>
 
         <TabsContent value="cat">Change your password here.</TabsContent>
         <TabsContent value="inspiration">a</TabsContent>
-        <TabsContent value="genearal">b</TabsContent>
+        <TabsContent value="general">b</TabsContent>
       </Tabs>
 
       {/* Mobile version */}
@@ -137,64 +119,25 @@ export function ArticleSection() {
             </SelectTrigger>
 
             <SelectContent>
-              <SelectItem value="highlight">Highlight</SelectItem>
-              <SelectItem value="cat">Cat</SelectItem>
-              <SelectItem value="inspiration">Inspiration</SelectItem>
-              <SelectItem value="general">General</SelectItem>
+              <CategorySelectItem />
             </SelectContent>
           </Select>
         </div>
       </div>
 
       <div className="row-auto grid grid-cols-1 gap-12 px-6 pt-6 lg:hidden">
-        <BlogCard
-          image={blogPosts[0].image}
-          category={blogPosts[0].category}
-          title={blogPosts[0].name}
-          description={blogPosts[0].description}
-          author={blogPosts[0].author}
-          date={blogPosts[0].date}
-        />
-        <BlogCard
-          image={blogPosts[1].image}
-          category={blogPosts[1].category}
-          title={blogPosts[1].name}
-          description={blogPosts[1].description}
-          author={blogPosts[1].author}
-          date={blogPosts[1].date}
-        />
-        <BlogCard
-          image={blogPosts[2].image}
-          category={blogPosts[2].category}
-          title={blogPosts[2].name}
-          description={blogPosts[2].description}
-          author={blogPosts[2].author}
-          date={blogPosts[2].date}
-        />
-        <BlogCard
-          image={blogPosts[3].image}
-          category={blogPosts[3].category}
-          title={blogPosts[3].name}
-          description={blogPosts[3].description}
-          author={blogPosts[3].author}
-          date={blogPosts[3].date}
-        />
-        <BlogCard
-          image={blogPosts[4].image}
-          category={blogPosts[4].category}
-          title={blogPosts[4].name}
-          description={blogPosts[4].description}
-          author={blogPosts[4].author}
-          date={blogPosts[4].date}
-        />
-        <BlogCard
-          image={blogPosts[5].image}
-          category={blogPosts[5].category}
-          title={blogPosts[5].name}
-          description={blogPosts[5].description}
-          author={blogPosts[5].author}
-          date={blogPosts[5].date}
-        />
+        {blogPosts.map((post) => {
+          return (
+            <BlogCard
+              image={post.image}
+              category={post.category}
+              title={post.name}
+              description={post.description}
+              author={post.author}
+              date={post.date}
+            />
+          );
+        })}
       </div>
 
       <div className="flex items-center justify-center p-6 py-14 lg:pb-28 lg:pt-20">
