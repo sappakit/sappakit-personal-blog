c30917b3ba485006411b8e521185e62f246e7de0
Sappakit <pipitwattana.s@gmail.com>
2024-10-23 23:24:19 +0700

    feat: add category selection feature

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import {
@@ -13,41 +15,82 @@ import { Search } from "lucide-react";
 import BlogCard from "./BlogCard";
 import { blogPosts } from "@/data/blogPosts";
 
-function CategoryTabsTrigger() {
-  const tabName = ["Highlight", "Cat", "Inspiration", "General"];
+function CategoryTabsTrigger({ tabName }) {
+  return (
+    <>
+      <div className="flex gap-2">
+        {tabName.map((name) => {
+          return (
+            <TabsTrigger
+              className="rounded-lg px-4 py-2 text-[--font-neutral-light-color] hover:bg-[--nav-bar-button-hover-color] data-[state=active]:bg-[var(--nav-bar-button-active-color)] data-[state=active]:text-[--font-neutral-dark-color]"
+              key={name}
+              value={name}
+            >
+              {name}
+            </TabsTrigger>
+          );
+        })}
+      </div>
+    </>
+  );
+}
 
+function CategorySelectItem({ tabName }) {
   return (
     <>
       {tabName.map((name) => {
-        const tabValue = name.toLowerCase();
-
         return (
-          <TabsTrigger
-            className="rounded-md px-4 py-2 text-[--font-neutral-light-color] data-[state=active]:bg-[var(--nav-bar-background-color)] data-[state=active]:text-[--font-neutral-dark-color]"
-            value={tabValue}
-          >
+          <SelectItem key={name} value={name}>
             {name}
-          </TabsTrigger>
+          </SelectItem>
         );
       })}
     </>
   );
 }
 
-function CategorySelectItem() {
-  const tabName = ["Highlight", "Cat", "Inspiration", "General"];
+function PostByCategory({ selectedCategory }) {
+  const filterCategory = blogPosts.filter(
+    (post) =>
+      selectedCategory === "Highlight" || post.category === selectedCategory,
+  );
 
   return (
-    <>
-      {tabName.map((name) => {
-        const selectValue = name.toLowerCase();
-        return <SelectItem value={selectValue}>{name}</SelectItem>;
+    <div className="row-auto grid grid-cols-1 gap-12 px-6 pt-6 lg:row-auto lg:grid-cols-2 lg:gap-5 lg:px-0 lg:pt-8">
+      {filterCategory.map((post) => {
+        return (
+          <BlogCard
+            key={post.id}
+            image={post.image}
+            category={post.category}
+            title={post.name}
+            description={post.description}
+            author={post.author}
+            date={post.date}
+          />
+        );
       })}
-    </>
+    </div>
+  );
+}
+
+function SearchInputBox() {
+  return (
+    <div className="search-input relative w-full lg:w-96">
+      <Input
+        className="w-full placeholder:font-semibold placeholder:text-[--font-neutral-light-color]"
+        type="text"
+        placeholder="Search"
+      />
+      <Search className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-[--font-neutral-light-color]" />
+    </div>
   );
 }
 
 export function ArticleSection() {
+  const tabName = ["Highlight", "Cat", "Inspiration", "General"];
+  const [selectedCategory, setSelectedCategory] = useState("Highlight");
+
   return (
     <section className="mx-auto flex w-full flex-col lg:max-w-screen-2xl lg:px-8">
       <h2 className="mb-6 px-6 text-xl font-bold text-[--font-primary-accent-color] lg:px-0">
@@ -56,88 +99,52 @@ export function ArticleSection() {
 
       {/* Desktop version */}
       <Tabs
-        defaultValue="highlight"
-        className="hidden flex-col items-center justify-between lg:flex"
+        value={selectedCategory}
+        onValueChange={(value) => setSelectedCategory(value)}
+        className="hidden lg:flex lg:flex-col"
       >
         <TabsList className="flex h-16 w-full items-center justify-between rounded-xl bg-[--bar-color] px-6">
           <div>
-            <CategoryTabsTrigger />
+            <CategoryTabsTrigger tabName={tabName} />
           </div>
 
-          <div className="search-input relative w-1/4">
-            <Input
-              className="w-full placeholder:font-semibold placeholder:text-[--font-neutral-light-color]"
-              type="text"
-              placeholder="Search"
-            />
-            <Search className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-[--font-neutral-light-color]" />
-          </div>
+          <SearchInputBox />
         </TabsList>
 
-        <TabsContent
-          className="row-auto m-0 grid grid-cols-2 gap-5 pt-12"
-          value="highlight"
-        >
-          {blogPosts.map((post) => {
-            return (
-              <BlogCard
-                image={post.image}
-                category={post.category}
-                title={post.name}
-                description={post.description}
-                author={post.author}
-                date={post.date}
-              />
-            );
-          })}
+        <TabsContent value={selectedCategory} className="m-0">
+          <div className="hidden lg:block">
+            <PostByCategory selectedCategory={selectedCategory} />
+          </div>
         </TabsContent>
-
-        <TabsContent value="cat">Change your password here.</TabsContent>
-        <TabsContent value="inspiration">a</TabsContent>
-        <TabsContent value="general">b</TabsContent>
       </Tabs>
 
       {/* Mobile version */}
       <div className="flex flex-col gap-3 bg-[--bar-color] px-6 py-4 lg:hidden">
-        <div className="search-input relative">
-          <Input
-            className="w-full placeholder:font-medium placeholder:text-[--font-neutral-light-color]"
-            type="text"
-            placeholder="Search"
-          />
-          <Search className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-[--font-neutral-light-color]" />
-        </div>
+        <SearchInputBox />
 
         <div className="flex flex-col gap-1">
           <p className="font-medium text-[--font-neutral-light-color]">
             Category
           </p>
 
-          <Select defaultValue="highlight">
+          <Select
+            value={selectedCategory}
+            onValueChange={(value) => setSelectedCategory(value)}
+          >
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Highlight" />
             </SelectTrigger>
 
             <SelectContent>
-              <CategorySelectItem />
+              <CategorySelectItem tabName={tabName} />
             </SelectContent>
           </Select>
         </div>
       </div>
 
-      <div className="row-auto grid grid-cols-1 gap-12 px-6 pt-6 lg:hidden">
-        {blogPosts.map((post) => {
-          return (
-            <BlogCard
-              image={post.image}
-              category={post.category}
-              title={post.name}
-              description={post.description}
-              author={post.author}
-              date={post.date}
-            />
-          );
-        })}
+      {/* Mobile post */}
+      <div className="block lg:hidden">
+        <PostByCategory selectedCategory={selectedCategory} />
       </div>
 
       <div className="flex items-center justify-center p-6 py-14 lg:pb-28 lg:pt-20">
